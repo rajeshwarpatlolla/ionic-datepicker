2afb2a02fb52676b69374900adedc21dc56ca066
Raphael M. Duarte Pinheiro <raphaklaus@gmail.com>
2015-06-30 21:42:55 -0300

    Today routine adjustment
    #29

--- src/ionic-datepicker.js
+++ src/ionic-datepicker.js
@@ -192,6 +192,10 @@ app.directive('ionicDatepicker', ['$ionicPopup', 'DatepickerService', function (
               onTap: function (e) {
 
                 var today = new Date();
+                today.setHours(0);
+                today.setMinutes(0);
+                today.setSeconds(0);
+                today.setMilliseconds(0);                     
                 var tempEpoch = new Date(today.getFullYear(), today.getMonth(), today.getDate());
                 var todayObj = {
                   date: today.getDate(),
