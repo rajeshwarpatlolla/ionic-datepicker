6f3fd1ce99ce66706f0918821a78a0d39353b1ef
rajeshwarpatlolla <rajeshwar.patlolla@gmail.com>
2015-08-17 00:47:00 +0530

    gulp tasks added

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,44 @@
-var path = require('path');
 var gulp = require('gulp');
-var clean = require('gulp-clean');
+var del = require('del');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var ngHtml2Js = require("gulp-ng-html2js");
 var minifyHtml = require("gulp-minify-html");
 var minifycss = require("gulp-minify-css");
+var css2js = require("gulp-css2js");
 
 gulp.task('html2js', function () {
-    gulp.src(['./src/*.html'])
-        .pipe(minifyHtml())
-        .pipe(ngHtml2Js({
-            moduleName: "ionic-datepicker.templates"
-        }))
-        .pipe(concat("templates.js"))
-        .pipe(uglify())
-        .pipe(gulp.dest("./dist"));
+  return gulp.src(['./src/*.html'])
+    .pipe(minifyHtml())
+    .pipe(ngHtml2Js({
+      moduleName: "ionic-datepicker.templates"
+    }))
+    .pipe(concat("templates.js"))
+    //.pipe(uglify())
+    .pipe(gulp.dest("./dist"));
 });
 
-gulp.task('build',['html2js','cssminify'], function () {
-    gulp.src(['./src/ionic-datepicker.js'])
-        .pipe(uglify())
-        .pipe(gulp.dest("./dist"));
+gulp.task('css2js', function () {
+  return gulp.src("./src/*.css")
+    .pipe(css2js())
+    //.pipe(uglify())
+    .pipe(gulp.dest("./dist/"));
 });
 
-gulp.task('cssminify', function () {
-    return gulp.src('./src/*.css')
-        .pipe(minifycss())
-        .pipe(gulp.dest('./dist'));
+gulp.task('make-bundle', ['del', 'html2js', 'css2js'], function () {
+  return gulp.src(['dist/*', './src/ionic-datepicker.js'])
+    .pipe(concat('ionic-datepicker.bundle.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('clean', function () {
-    return gulp.src('dist', {read: false})
-        .pipe(clean());
+gulp.task('del-temp-files', ['make-bundle'], function () {
+  del(['dist/templates.js', 'dist/style.js']);
 });
 
-gulp.task('default',['clean','build']);
+gulp.task('del', function () {
+  del(['dist/*']);
+});
+
+gulp.task('build', ['del-temp-files']);
+
