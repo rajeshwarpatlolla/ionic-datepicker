730a04368e64d2ac3255e9b771ee474777504330
gyleg5 <gyleg5@gmail.com>
2016-09-19 01:47:48 -0400

    Added closePopup function
    Added ability to manually close popup.  Useful if another popup needs to take over or timer function changes screens automatically.

---
+++ ionic-datepicker.service.js
@@ -0,0 +1,21 @@
+angular.module('ionic-datepicker.service', [])
+
+  .service('IonicDatepickerService', function () {
+
+    this.monthsList = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    this.getYearsList = function (from, to) {
+      var yearsList = [];
+      var minYear = 1900;
+      var maxYear = 2100;
+
+      minYear = from ? new Date(from).getFullYear() : minYear;
+      maxYear = to ? new Date(to).getFullYear() : maxYear;
+
+      for (var i = minYear; i <= maxYear; i++) {
+        yearsList.push(i);
+      }
+
+      return yearsList;
+    };
+  });
