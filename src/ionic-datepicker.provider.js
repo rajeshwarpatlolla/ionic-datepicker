bb75e28c7b9761f2d2f3c1368bac9f80065bd8b4
Rich Christiansen <rich@byu.net>
2016-10-14 15:50:15 -0600

    Comment out stray console.log()

--- src/ionic-datepicker.provider.js
+++ src/ionic-datepicker.provider.js
@@ -265,7 +265,7 @@ angular.module('ionic-datepicker.provider', [])
           text: $scope.mainObj.closeLabel,
           type: 'button_close',
           onTap: function (e) {
-            console.log('ionic-datepicker popup closed.');
+            // console.log('ionic-datepicker popup closed.');
           }
         });
 
